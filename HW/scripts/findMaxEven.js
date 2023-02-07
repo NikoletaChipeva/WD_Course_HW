78d6575d1654857faff43106a51c9e0cf66811ae
NikoletaChipeva <116991790+NikoletaChipeva@users.noreply.github.com>
2023-02-07 13:42:34 +0200

    Add files via upload

---
+++ HW/scripts/findMaxEven.js
@@ -0,0 +1,8 @@
+// TASK: да се дефинира функцията findMaxEven(), която да връща максималното четно число от подаденият й масив.
+
+// ---> YOUR CODE HERE <---
+
+// TEST
+let numbers = [2, -4, 5, 3, 9, 0, 1];
+let max = findMaxEven(numbers);
+console.log(`The max of ${numbers} is ${max}`);
