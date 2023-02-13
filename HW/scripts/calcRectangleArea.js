221624a925d624de019779adc2c472ce02d81288
NikoletaChipeva <116991790+NikoletaChipeva@users.noreply.github.com>
2023-02-13 09:51:04 +0200

    Add files via upload

---
+++ HW/scripts/calcRectangleArea.js
@@ -0,0 +1,21 @@
+//TASK: да се дефинира функция calcRectangleArea(), която връща лицето на правоъгълник, със зададени като пропъртита на обект дължина и ширина:
+
+// ---> YOUR CODE HERE <---
+
+// TEST:
+let rectangle = {
+    width: 6,
+    height: 3
+  };
+
+  let calcRectangleArea = function(rectangle) {
+    let area=rectangle.width*rectangle.height;
+    return area;
+}
+
+  let area = calcRectangleArea(rectangle);
+  
+  console.log(`The area of rectangle is: ${area}`);
+  
+  // expected result:
+  // The area of rectangle is: 18
